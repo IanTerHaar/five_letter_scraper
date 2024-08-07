fb1f94313a4112b4aebfdc21c036fe04592985f9
Ian Ter Haar <ian.terhaar1004@gmail.com>
2024-08-07 21:29:34 +0200

    add code for scraper

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String inputFilePath = "src\\words.txt";
+        String outputFilePath = "output.txt";
+
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))
+        ) {
+            String word;
+            while ((word = reader.readLine()) != null) {
+                if (isValidWord(word)) {
+                    writer.write(word);
+                    writer.newLine();
+                }
+            }
+            System.out.println("Words have been filtered and written to " + outputFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isValidWord(String word) {
+        
+        return word.length() == 5 && word.matches("[a-zA-Z]+");
+    }
+}
